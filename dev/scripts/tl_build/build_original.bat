39a751d1ed7ba7d4d0194476ebe8de980d69b9fb
sowmyasw <sowmya.swaminathan@gteamstaff.com>
2016-10-20 16:06:47 +0530

    Executable_batAnDsh

--- dev/scripts/tl_build/build_original.bat
+++ dev/scripts/tl_build/build_original.bat
@@ -1,41 +1,26 @@
-SET PATH=%PATH%;C:\Program Files\Java\jdk1.8.0_40\bin;
-
-cd %BASE_DIR%\sources\QAProduct\dev\src
+cd %BASE_DIR%\sources\QAProduct\dev\src\src\com\devfactory\qa\sample
 
 echo "Changed directory to sources"
 
-call ant %DF_ANT_BUILD_STATS_OPTS% all -debug || echo "Error: Build failed" && exit /b 1; 
+javac -cp "%BASE_DIR%\sources\QAProduct\dev\src\lib\javax.servlet_3.0.0.v201112011016.jar"  HelloWorld.java || echo "Error: Build failed" && exit /b 1; 
 
-REM The compiled class files are available under dev\src\out directory afte ant successfully executes
-
-mkdir $BASE_DIR\output
-
-mkdir $BASE_DIR\output\installers
-
-echo "Ant completed successfully"
-
-cd %BASE_DIR%\sources\QAProduct\
-call "C:\Program Files\7-Zip\7z.exe" a -r -tzip %BASE_DIR%\output\installers\QAPinstaller.zip *
-
+echo "Compilation completed successfully"
 
 mkdir %BASE_DIR%\output\binaries
-cd %BASE_DIR%\sources\QAProduct\dev\src\out
-jar cvf QAProduct1.jar ./com/devfactory/qa/sample/HelloWorld.class
+xcopy %BASE_DIR%\sources\QAProduct\dev\src\src\com\devfactory\qa\sample\HelloWorld.class %BASE_DIR%\output\binaries /s /e
 
-xcopy %BASE_DIR%\sources\QAProduct\dev\src\out %BASE_DIR%\output\binaries /s /e
+mkdir %BASE_DIR%\output\installers
+xcopy %BASE_DIR%\sources\QAProduct\dev\src\src\com\devfactory\qa\sample\HelloWorld.class %BASE_DIR%\output\installers /s /e
 
 REM -------------------------------------For UT----------------------------------------------
-REM jar cf QAProduct.jar .\*
-xcopy %BASE_DIR%\sources\QAProduct\dev\src\out\QAProduct1.jar %BASE_DIR%\sources\QAProduct\nonuitest\JUnit\Testing_Framework\extras\lib 
-xcopy %BASE_DIR%\sources\QAProduct\dev\src\out\QAProduct1.jar %BASE_DIR%\sources\QAProduct\nonuitest\TestNG\lib 
+xcopy %BASE_DIR%\sources\QAProduct\dev\src\src\com\devfactory\qa\sample\HelloWorld.class %BASE_DIR%\sources\QAProduct\nonuitest\JUnit\Testing_Framework\extras\lib 
+xcopy %BASE_DIR%\sources\QAProduct\dev\src\src\com\devfactory\qa\sample\HelloWorld.class %BASE_DIR%\sources\QAProduct\nonuitest\TestNG\lib 
 
 mkdir %BASE_DIR%\output\libraries
 xcopy %BASE_DIR%\sources\QAProduct\dev\src\lib %BASE_DIR%\output\libraries /s /e
 
 mkdir %BASE_DIR%\output\sources
 xcopy %BASE_DIR%\sources\QAProduct\dev\src\src %BASE_DIR%\output\sources /s /e
-
-mkdir %BASE_DIR%\output\unittests
 
 if errorlevel 1 (
 echo Failure Reason Given is %errorlevel%
