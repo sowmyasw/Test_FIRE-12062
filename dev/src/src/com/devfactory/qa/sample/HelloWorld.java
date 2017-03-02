6c9bc2e5443b048b53cf735b359483ae4a3ce31e
sowmyasw <sowmya.swaminathan@gteamstaff.com>
2017-03-02 17:09:35 +0530

    Synch Initial Code of Git of Test product with that of SVN repo

--- dev/src/src/com/devfactory/qa/sample/HelloWorld.java
+++ dev/src/src/com/devfactory/qa/sample/HelloWorld.java
@@ -2,15 +2,12 @@ package com.devfactory.qa.sample;
 
 import java.util.Date;
 import java.lang.*;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ArrayList;
 
 //Basic simple only product Class 
 //Always returns the string  "Hello  World" 
 
 public class HelloWorld{
-    //public method to return hardcoded "HelloWorld" Stringsssssssddfad
+    //public method to return hardcoded "HelloWorld" String
 	public static final String var="Dummy";
 	private Date var2;
     public String printHelloWorld(){
@@ -39,22 +36,6 @@ public class HelloWorld{
 	
 	public void M4()
 	{
-		return;
-	}
-	
-	public void M5(){}
-	
-	public void testNonBlocker(){
-		try{
-			List<String> list = new ArrayList<String>();
-			Integer integer = Integer.valueOf(1);
-
-			if (list.contains(integer)) { // Noncompliant. Always false.
-			list.remove(integer); // Noncompliant. list.add(iger) doesn't compile, so this will always return false
-			}
-		}catch(Exception e){
-			e.printStackTrace();
-		}
 		return;
 	}
 	
