9c020d93db30a9ac26d3381a2e31af0208f042dc
sowmyasw <sowmya.swaminathan@gteamstaff.com>
2017-03-31 11:16:32 +0530

    Commit Uniformly formatted source files

--- dev/src/src/com/devfactory/qa/sample/HelloWorld.java
+++ dev/src/src/com/devfactory/qa/sample/HelloWorld.java
@@ -6,37 +6,36 @@ import java.lang.*;
 //Basic simple only product Class 
 //Always returns the string  "Hello  World" 
 
-public class HelloWorld{
-    //public method to return hardcoded "HelloWorld" String
-	public static final String var="Dummy";
-	private Date var2;
-    public String printHelloWorld(){
-		//To fire commit
-        return "HelloWorld";
-    }
-	
-	/*
-	public void method2(Date input){
-		this.var2 = input;
-	}
-	*/
-	//
-	public void M1()
-	{
-	
-	}
-	
-	public void M2()
-	{}
-	
-	public void M3()
-	{
-		return;
-	}
-	
-	public void M4()
-	{
-		return;
-	}
-	
+public class HelloWorld {
+
+  //public method to return hardcoded "HelloWorld" String
+  public static final String var = "Dummy";
+  private Date var2;
+
+  public String printHelloWorld() {
+    //To fire commit
+    return "HelloWorld";
+  }
+
+  /*
+  public void method2(Date input){
+    this.var2 = input;
+  }
+  */
+  //
+  public void M1() {
+
+  }
+
+  public void M2() {
+  }
+
+  public void M3() {
+    return;
+  }
+
+  public void M4() {
+    return;
+  }
+
 }
