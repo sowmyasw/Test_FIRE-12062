828f9c067e7df8beebfb7c6d3e34d9d081c87681
sowmyasw <sowmya.swaminathan@gteamstaff.com>
2017-05-18 15:18:41 +0530

    Fix OpenPojo test with Alex's comments

--- dev/src/src/test/java/com/devfactory/qa/sample/test/TestHelloWorld_OpenPojo.java
+++ dev/src/src/test/java/com/devfactory/qa/sample/test/TestHelloWorld_OpenPojo.java
@@ -4,11 +4,10 @@ package com.devfactory.qa.sample.test;
 import com.devfactory.qa.sample.HelloWorld;
 import com.openpojo.reflection.PojoClass;
 import com.openpojo.reflection.impl.PojoClassFactory;
-import com.openpojo.validation.PojoValidator;
+import com.openpojo.validation.Validator;
+import com.openpojo.validation.ValidatorBuilder;
 import com.openpojo.validation.rule.impl.GetterMustExistRule;
 import com.openpojo.validation.rule.impl.SetterMustExistRule;
-import com.openpojo.validation.test.impl.GetterTester;
-import com.openpojo.validation.test.impl.SetterTester;
 import org.junit.Test;
 
 public class TestHelloWorld_OpenPojo {
@@ -16,18 +15,13 @@ public class TestHelloWorld_OpenPojo {
   public void testHelloWorld(){
 
 
-    PojoClass personPojo = PojoClassFactory.getPojoClass(HelloWorld.class);
+    PojoClass helloWorldPojo = PojoClassFactory.getPojoClass(HelloWorld.class);
 
-    PojoValidator pojoValidator = new PojoValidator();
+    Validator validator = ValidatorBuilder.create()
+        .with(new GetterMustExistRule(), new SetterMustExistRule())
+        .build();
 
-    pojoValidator.addRule(new SetterMustExistRule());
-    pojoValidator.addRule(new GetterMustExistRule());
-
-    pojoValidator.addTester(new SetterTester());
-    pojoValidator.addTester(new GetterTester());
-
-    pojoValidator.runValidation(personPojo);
-
+    validator.validate(helloWorldPojo);
   }
 
 }
